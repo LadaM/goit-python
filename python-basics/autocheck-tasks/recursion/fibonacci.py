a30623dd9a9403a389c0fa2883a0b995693b634b
Lada <lada11lada@gmail.com>
2023-10-20 16:43:23 +0200

    improve fibonacci implementation

--- python-basics/autocheck-tasks/recursion/fibonacci.py
+++ python-basics/autocheck-tasks/recursion/fibonacci.py
@@ -1,12 +1,8 @@
 def fibonacci(n):
-    if n == 0: 
+    if n == 0:
         return 0
     elif n == 1:
         return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(1))
-print(fibonacci(2))
-print(fibonacci(3))
-print(fibonacci(4))
+print(fibonacci(100))
